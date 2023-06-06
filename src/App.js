ec2648c31ba6ea9a1d88599420f1b5923ef363ae
Sean Gayle <sean@knightfoxapps.com>
2023-06-06 16:02:00 -0500

    cleaned up the switch tabs a bit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="mainPage">
-      <Header title="SW-API Deck Builder" updateTab={updateTab} />
+      <Header title="SW-API Deck Builder" updateTab={updateTab} showDecks={showDecks} />
       <h1>We are in the {showDecks ? 'Decks' : 'Cards'}</h1>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,15 @@ import { React, useState } from 'react'
 import NameTag from './NameTag'
 
 const Header = (props) => {
-    let [showDecks, switchTabs = (isDecks) => {
-        showDecks = isDecks
-    }] = useState(false);
 
-    const goToDecks = () => {
-        switchTabs(true)
-        props.updateTab(true)
-    }
+    const goToDecks = () => props.updateTab(true)
 
-    const goToCards = () => {
-        switchTabs(false)
-        props.updateTab(false)
-    }
+    const goToCards = () => props.updateTab(false)
     return (
         <div >
             <div className="header">
                 <div>
-                    <button className='iconbtn' onClick={goToCards} style={{ backgroundColor: !showDecks ? '#fff' : '#B8B8B8' }}>
+                    <button className='iconbtn' onClick={goToCards} style={{ backgroundColor: !props.showDecks ? '#fff' : '#B8B8B8' }}>
                         <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <g clip-path="url(#clip0_12_2576)">
                                 <path d="M10 12.5V9.99998C10 9.60216 9.84196 9.22063 9.56066 8.93932C9.27936 8.65802 8.89782 8.49998 8.5 8.49998V8.49998C8.10218 8.49998 7.72064 8.65802 7.43934 8.93932C7.15804 9.22063 7 9.60216 7 9.99998V13.5C7.05225 14.001 7.21362 14.4844 7.47277 14.9163C7.73192 15.3482 8.08255 15.7181 8.5 16H12L13.348 11.508C13.4473 11.1749 13.4587 10.8218 13.381 10.483L12.181 5.27398C12.0663 4.77664 11.7658 4.34196 11.341 4.05898L10.5 3.49998" stroke="#3B3B3B" stroke-linecap="round" stroke-linejoin="round" />
@@ -34,7 +25,7 @@ const Header = (props) => {
                         &nbsp;All Cards
 
                     </button>
-                    <button className='iconbtn' onClick={goToDecks} style={{ backgroundColor: showDecks ? '#fff' : '#B8B8B8' }}>
+                    <button className='iconbtn' onClick={goToDecks} style={{ backgroundColor: props.showDecks ? '#fff' : '#B8B8B8' }}>
                         <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M8 1.99998L15.5 6.49998L8 11L0.5 6.49998L8 1.99998Z" stroke="#3B3B3B" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
                             <path d="M14 9.59999L15.5 10.5L8 15L0.5 10.5L2 9.59999" stroke="#3B3B3B" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round" />
